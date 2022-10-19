559d2a3076f357f0f9726ecbc27f5297f7f242cf
YuryShageev <schageev.yur@yandex.ru>
2022-10-19 12:32:35 +0300

    Первое задание готово

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,16 @@ public class Main {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
-        System.out.printf("ФИО сотрудника - %s %s %s", lastName,  firstName, middleName);
+        System.out.printf("ФИО сотрудника - %s %s %s", lastName, firstName, middleName);
+        System.out.println();
+        task2();
     }
+
+    public static void task2() {
+        System.out.println("Task 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника - " + upperCaseFullName);
+    }
+
 }
